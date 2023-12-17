1bd6691d9c5fd1b4e456688a01e841ac69a86d13
VIPon4ik <umniyd@gmail.com>
2023-12-17 16:24:16 +0200

    Added error message in PrivateRoute and PublicRoute

--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -2,10 +2,18 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { Outlet, Navigate } from 'react-router-dom';
 import { selectIsLogged } from '../../redux/selectors';
+import { toast } from 'react-toastify';
 
 const PrivateRoutes = () => {
   const isLogged = useSelector(selectIsLogged);
-  return isLogged ? <Outlet /> : <Navigate to="/login" />;
+  return isLogged ? (
+    <Outlet />
+  ) : (
+    <>
+      {toast.error('You need to sign in')}
+      <Navigate to="/login" />
+    </>
+  );
 };
 
 export default PrivateRoutes;
--- src/components/PublicRoute/PublicRoute.jsx
+++ src/components/PublicRoute/PublicRoute.jsx
@@ -2,10 +2,18 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { Outlet, Navigate } from 'react-router-dom';
 import { selectIsLogged } from '../../redux/selectors';
+import { toast } from 'react-toastify';
 
 const PublicRoutes = () => {
   const isLogged = useSelector(selectIsLogged);
-  return isLogged ? <Navigate to="/" /> : <Outlet />;
+  return isLogged ? (
+    <>
+      {toast.error('You already logged in ')}
+      <Navigate to="/" />
+    </>
+  ) : (
+    <Outlet />
+  );
 };
 
 export default PublicRoutes;
